80252aafb3baa2e5a1ac943afeb43bba505bd90c
user04 <user04@gmail.com>
2026-08-11 12:19:34 +0530

    controller

--- src/api/alb-blog/controllers/alb-blog.js
+++ src/api/alb-blog/controllers/alb-blog.js
@@ -6,5 +6,77 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::alb-blog.alb-blog');
+// module.exports = createCoreController('api::alb-blog.alb-blog');
+
+module.exports = createCoreController('api::alb-blog.alb-blog', ({ strapi }) => ({
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                Banner: true,
+                metaTags: true,
+                weatherInfo: {
+                    populate: {
+                        currentWeatherIcon: true,
+                        bestTimeIcon: true,
+                        targetAudienceIcon: true,
+                    },
+                },
+                destinationPackages: {
+                    populate: {
+                        image: true,
+                    },
+                },
+                highlightExperiences: {
+                    populate: {
+                        image: true,
+                    },
+                },
+                relaredBlog: {
+                    populate: {
+                        Banner: true,
+                    },
+                },
+            },
+        };
+
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+    },
+
+    async findOne(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                Banner: true,
+                metaTags: true,
+                weatherInfo: {
+                    populate: {
+                        currentWeatherIcon: true,
+                        bestTimeIcon: true,
+                        targetAudienceIcon: true,
+                    },
+                },
+                destinationPackages: {
+                    populate: {
+                        image: true,
+                    },
+                },
+                highlightExperiences: {
+                    populate: {
+                        image: true,
+                    },
+                },
+                relaredBlog: {
+                    populate: {
+                        Banner: true,
+                    },
+                },
+            },
+        };
+
+        const { data, meta } = await super.findOne(ctx);
+        return { data, meta };
+    },
+}));
 
