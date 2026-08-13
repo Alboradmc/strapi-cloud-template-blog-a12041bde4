3ad02fa65b6d34c03ef03648bf465f74b67cee1f
user04 <user04@gmail.com>
2026-08-13 17:44:52 +0530

    controller changes

--- src/api/alb-usp-list/controllers/alb-usp-list.js
+++ src/api/alb-usp-list/controllers/alb-usp-list.js
@@ -6,36 +6,6 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::alb-usp-list.alb-usp-list', ({ strapi }) => ({
-    async find(ctx) {
-        const populate = ctx.query.populate;
-
-        if (populate === '*' || populate === 'details' || (typeof populate === 'object' && populate && populate.details)) {
-            ctx.query.populate = {
-                ...(typeof populate === 'object' && populate && !Array.isArray(populate) ? populate : {}),
-                details: {
-                    populate: '*',
-                },
-            };
-        }
-
-        return super.find(ctx);
-    },
-
-    async findOne(ctx) {
-        const populate = ctx.query.populate;
-
-        if (populate === '*' || populate === 'details' || (typeof populate === 'object' && populate && populate.details)) {
-            ctx.query.populate = {
-                ...(typeof populate === 'object' && populate && !Array.isArray(populate) ? populate : {}),
-                details: {
-                    populate: '*',
-                },
-            };
-        }
-
-        return super.findOne(ctx);
-    },
-}));
+module.exports = createCoreController('api::alb-usp-list.alb-usp-list');
 
 
