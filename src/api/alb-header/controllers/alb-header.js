ccb5b584f720c03c3fdb6f67cb2649bac9dda379
user04 <user04@gmail.com>
2026-09-17 17:19:28 +0530

    some changes

--- src/api/alb-header/controllers/alb-header.js
+++ src/api/alb-header/controllers/alb-header.js
@@ -6,5 +6,47 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::alb-header.alb-header');
+module.exports = createCoreController('api::alb-header.alb-header', () => ({
+    /** @param {any} ctx */
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                topMenu: {
+                    populate: {
+                        Icon: true,
+                    },
+                },
+                serviceMenu: {
+                    populate: {
+                        Icon: true,
+                    },
+                },
+            },
+        };
+
+        return super.find(ctx);
+    },
+
+    /** @param {any} ctx */
+    async findOne(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            populate: {
+                topMenu: {
+                    populate: {
+                        Icon: true,
+                    },
+                },
+                serviceMenu: {
+                    populate: {
+                        Icon: true,
+                    },
+                },
+            },
+        };
+
+        return super.findOne(ctx);
+    },
+}));
 
